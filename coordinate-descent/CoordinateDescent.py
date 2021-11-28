848cc3b08b84e31c29a1ef9d94494cca8332e77e
felixbastian <felixbastian.hans@gmail.com>
2021-11-28 12:01:42 +0100

    basic model working - FH

--- coordinate-descent/CoordinateDescent.py
+++ coordinate-descent/CoordinateDescent.py
@@ -2,19 +2,23 @@ import numpy as np
 
 class CoordinateDescent:
 
-    def __init__(self, PARAMETERS):
+    def costf(X, y, param):
+        return np.sum((X.dot(param) - y) ** 2) / (2 * len(y))
 
     #Import from stackexchange
     #https://stackoverflow.com/questions/51977418/coordinate-descent-in-python
+
     def coordinate_descent(X, y, param, iter=300):
+
+
         cost_history = [0] * (iter+1)
-        cost_history[0] = costf(X, y, param)
+        cost_history[0] = CoordinateDescent.costf(X, y, param)
 
         for iteration in range(iter):
             for i in range(len(param)):
                 dele = np.dot(np.delete(X, i, axis=1), np.delete(param, i, axis=0))
                 param[i] = np.dot(X[:,i].T, (y.ravel() - dele))/np.sum(np.square(X[:,i]))
-                cost = costf(X, y, param)
+                cost = CoordinateDescent.costf(X, y, param)
                 cost_history[iteration+1] = cost
 
         return param, cost_history
--- coordinate-descent/main.py
+++ coordinate-descent/main.py
@@ -1,11 +1,14 @@
 #As provided on stackoverflow, we can use the dataset to test out model
-import matplotlib as plt
-from sklearn.datasets import load_boston
+import matplotlib.pyplot as plt
+import CoordinateDescent as cd
+from sklearn.datasets import fetch_california_housing
 from sklearn.preprocessing import StandardScaler
 import numpy as np
+import pandas
 
-#  boston house-prices dataset
-data = load_boston()
+#  california house-prices dataset
+data = fetch_california_housing(as_frame=True)
+
 X, y = data.data, data.target
 
 X = StandardScaler().fit_transform(X)  # for easy convergence
@@ -15,9 +18,7 @@ param = np.zeros(X.shape[1])
 
 
 #Here the function "coordinate_descent gets called that we need to refer to
+cret, cxret = cd.CoordinateDescent.coordinate_descent(X, y, param.copy())
 
-cret, cxret = coordinate_descent(X, y, param.copy())
-
-plt.plot(range(len(xret)), xret, label="GD")
 plt.plot(range(len(cxret)), cxret, label="CD")
-plt.legend()
+plt.show()
