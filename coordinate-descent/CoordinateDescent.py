42223686b2cbb37f20f519d64c9f4e462e30ff69
felixbastian <felixbastian.hans@gmail.com>
2021-11-29 14:08:38 +0100

    ...

--- coordinate-descent/CoordinateDescent.py
+++ coordinate-descent/CoordinateDescent.py
@@ -10,16 +10,15 @@ class CoordinateDescent:
 
     def coordinate_descent(X, y, param, iter=300):
 
-
         cost_history = [0] * (iter+1)
         cost_history[0] = CoordinateDescent.costf(X, y, param)
-
+        print(cost_history)
         for iteration in range(iter):
             for i in range(len(param)):
                 dele = np.dot(np.delete(X, i, axis=1), np.delete(param, i, axis=0))
                 param[i] = np.dot(X[:,i].T, (y.ravel() - dele))/np.sum(np.square(X[:,i]))
-                cost = CoordinateDescent.costf(X, y, param)
-                cost_history[iteration+1] = cost
+            cost = CoordinateDescent.costf(X, y, param)
+            cost_history[iteration+1] = cost
 
         return param, cost_history
 
--- coordinate-descent/main.py
+++ coordinate-descent/main.py
@@ -11,11 +11,14 @@ data = fetch_california_housing(as_frame=True)
 
 X, y = data.data, data.target
 
+print(X.shape[1])
+
 X = StandardScaler().fit_transform(X)  # for easy convergence
 X = np.hstack([X, np.ones((X.shape[0], 1))])
 
 param = np.zeros(X.shape[1])
 
+print(X.dot(param).shape)
 
 #Here the function "coordinate_descent gets called that we need to refer to
 cret, cxret = cd.CoordinateDescent.coordinate_descent(X, y, param.copy())
