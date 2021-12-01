2803f11361aa7f62bf05f4d9108756b3ee0fde98
MariaIsabelVeraC <77512419+MariaIsabelVeraC@users.noreply.github.com>
2021-12-01 06:32:09 +0100

    Update main.py

--- coordinate-descent/main.py
+++ coordinate-descent/main.py
@@ -1,10 +1,12 @@
 #As provided on stackoverflow, we can use the dataset to test out model
 import matplotlib.pyplot as plt
 import CoordinateDescent as cd
+import sklearn
 from sklearn.datasets import fetch_california_housing
 from sklearn.preprocessing import StandardScaler
 import numpy as np
 import pandas
+import ElasticNetCD as encd
 
 #  california house-prices dataset
 data = fetch_california_housing(as_frame=True)
@@ -19,6 +21,14 @@ param = np.zeros(X.shape[1])
 
 #Here the function "coordinate_descent gets called that we need to refer to
 cret, cxret = cd.CoordinateDescent.coordinate_descent(X, y, param.copy())
+print(cret)
 
 plt.plot(range(len(cxret)), cxret, label="CD")
+
+#Elastic net using coordinate descent
+bstar = encd.ElasticNetCD.elastic_net(X, y, 0.2, 0.3)
+print(bstar)
+
+
+
 plt.show()
