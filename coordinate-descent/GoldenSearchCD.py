8203f8005420366dcf7d296b71f4c903e5c4a77b
Miao-WANG-Trista <b00761718@essec.edu>
2021-12-03 20:38:02 +0100

    added some comments

--- coordinate-descent/GoldenSearchCD.py
+++ coordinate-descent/GoldenSearchCD.py
@@ -15,7 +15,7 @@ def check_e(xu, xl, xopt, absolutePrecision=1e-10):
     :param xu: upper bound of the interval
     :param xl: lower bound of the interval
     :param xopt: minimal values of x
-    :param absolutePrecision: the stopping precision. Default = 1e-10
+    :param absolutePrecision: the stopping precision,tolerance. Default = 1e-10
     :return: whether the stopping precision is reached 
     """
     ea = (1 - 0.618) * abs((xu - xl) / xopt)
@@ -27,11 +27,11 @@ def check_e(xu, xl, xopt, absolutePrecision=1e-10):
 def gold_search(func, xu, xl, absolutePrecision=1e-10, itr=1000):
     """
     :param func: objective function
-    :param n: degree of objective function
-    :param lower_bound: lower_bound of search interval
-    :param higher_bound: higher_bound of search interval
-    :param max_iter: times of iterations to stop algorithm once reached
-    :return: minimal values of x
+    :param xu: upper bound of the interval
+    :param xl: lower bound of the interval
+    :param absolutePrecision: the stopping precision, tolerance. Default = 1e-10
+    :param itr: maximum iteration times
+    :return: optimal values of x in a minimum problem
     """
     x = [0, 0]
     f = [0, 0]
@@ -92,8 +92,10 @@ class CDGoldenSearch:
         """
         Conducts coordinate descent 
         """
-        # random initialization 
+        # random initialization
+        # here, we use a list to store values in different coordinates
         init_X = list(range(self.n))
+
         X = [round(random.uniform(self.lower_bound,self.higher_bound),2) 
              for _ in range(self.n) ]
         for i in range(self.n):
@@ -107,7 +109,6 @@ class CDGoldenSearch:
                 list_p = opt_X[0:k] + opt_X[k+1:self.n]
                 opt_X[k] = gold_search(lambda x: self.func(x, k, list_p, self.n),
                                      self.lower_bound,self.higher_bound)
-        
         
         return opt_X
         
